f4a044440e6955efe096d335d73f1061e5282609
Abusch419 <abusch419@gmail.com>
2019-06-20 15:01:41 -0400

    cat cards working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import NavComponent from "./components/NavComponent"
+import Category from './components/Category';
 
-function App() {
-  return (
-    <>
+class App extends Component {
+
+state = {
+  categories: [
+    {
+      image: "http://placekitten.com/300/300",
+      title: "Pets",
+      icon: "paw"
+  },
+  ]
+}
+
+  // componentDidMount() {
+  //      this.loadCategories();
+  //  }
+
+  //  loadCategories = () => {
+  //     API.getCategories()
+  //     .then(res =>
+  //       this.setState({ category: res.data, image: "", icon: ""})
+  //     )
+  //     .catch(err => console.log(err));
+  //  }
+
+
+
+  render () {
+    return (
+      <>
       <NavComponent/>
-    
+      {this.state.categories.map(category => (
+        <Category 
+      image={category.image}
+      title={category.title}
+      icon={category.icon}
+      /> 
+      ))}
     </>
-  );
+    )
+  
+  };
 }
 
 export default App;
