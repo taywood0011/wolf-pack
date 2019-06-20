4c68436331d7b2e3f205f4a7403a9eb361fdcb7d
amp3193 <amp3193@yahoo.com>
2019-06-20 15:00:20 -0400

    login/context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import ProtectedRoute from "./components/LoginComponent/ProtectedRoute";
 import NavComponent from "./components/NavComponent"
+import LoginComponent from "./components/LoginComponent"
+import UserContext from "./context/UserContext";
+import HomePage from "./components/LoginComponent/Homepage";
 
-function App() {
+
+class App extends Component {
+  state = {
+    user: null
+  }
+
+  setUser = (user) => {
+	  this.setState({ user });
+  } 
+
+ render() {
+  const {user} = this.state;
+	const setUser = this.setUser;
   return (
+    <Router>
     <>
-      <NavComponent/>
+      <LoginComponent> 
+      	<UserContext.Provider value={{ setUser, user }}>
+          <ProtectedRoute exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginComponent} />
+       </UserContext.Provider>
+       </LoginComponent>
+      <NavComponent />
     
     </>
+    </Router>
   );
+} 
 }
 
 export default App;
--- src/components/LoginComponent/index.js
+++ src/components/LoginComponent/index.js
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import UserContext from "../../context/UserContext";
+import Auth from "../../utils/Auth";
 import { Form, FormInput, FormGroup, Button } from "shards-react";
 
-export default class LoginComponent extends React.Component() {
 
- render() {
-  return (
-    <Form>
-      <FormGroup>
-        <label htmlFor="#username">Username</label>
-        <FormInput id="#username" placeholder="Username" />
-      </FormGroup>
-      <FormGroup>
-        <label htmlFor="#password">Password</label>
-        <FormInput type="password" id="#password" placeholder="Password" />
-      </FormGroup>
-      <Button theme="dark">Login</Button>
-      <Button theme="dark">Create</Button>
-    </Form>
-  );
+class LoginComponent extends Component {
+    static contextType = UserContext;
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    changeHandler = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault();
+        const { username, password } = this.state;
+        if (username && password) {
+            Auth.logIn(username, password, (response) => {
+                this.context.setUser(response);
+                this.props.history.push("/");
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Wolf-Pack</h2>
+                <Form onSubmit={this.submitHandler}>
+                    <FormGroup>
+                        <label htmlFor="username">Username</label>
+                        <FormInput
+                            id="username"
+                            name='username'
+                            placeholder="Username"
+                            type="text"
+                            value={this.state.username}
+                            onChange={this.changeHandler}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor="password">Password</label>
+                        <FormInput
+                            id="password"
+                            type="password"
+                            name='password'
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.changeHandler} />
+                    </FormGroup>
+                    <Button theme="dark" type="submit">Login</Button>
+                    <Button theme="dark">Create</Button>
+                </Form>
+            </div>
+        )
+    }
 }
-}
+
+export default withRouter(LoginComponent);
