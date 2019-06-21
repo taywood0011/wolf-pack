acc514dd51be1f1f5c172295b30af82789242098
Tyler Mathena <tgmathena@crimson.ua.edu>
2019-06-20 20:32:37 -0400

    made root page less ugly with a big empty div and background

--- src/components/Tundra/index.js
+++ src/components/Tundra/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container} from "shards-react";
 import ItemCard from "../ItemCard";
+import "./styles.css"
 
 class Tundra extends React.Component {
 
@@ -29,20 +30,22 @@ class Tundra extends React.Component {
         this.setState({
           data: [
             {
+              id: 1,
               img: "",
               title: "Looking for group!!!1!",
               body:
                 "ISO an airbud/electro-swing fan group. Both would be ideal, but either would work.",
               btnAction: "Invite",
-              clickFn: this.inviteHowl
+              type: "howl"
             },
             {
+              id: 2,
               img: "",
               title: "Looking for group!!!1!",
               body:
                 "ISO an airbud/electro-swing fan group. Both would be ideal, but either would work.",
               btnAction: "Invite",
-              clickFn: this.inviteHowl
+              type: "howl"
             }
           ]
         });
@@ -67,20 +70,22 @@ class Tundra extends React.Component {
         this.setState({
           data: [
             {
+              id: 1,
               img: "",
               title: "Forming Electro-Swing Band",
               body:
                 "We need a good keyboardist and of course we are always looking for other bands to practice, preform, or even just hang out with. Don't be shy!",
               btnAction: "Join", 
-              clickFn: this.joinGroup
+              type: "group"
             },
             {
+              id: 2,
               img: "",
               title: "Forming Electro-Swing Band",
               body:
                 "We need a good keyboardist and of course we are always looking for other bands to practice, preform, or even just hang out with. Don't be shy!",
               btnAction: "Join",
-              clickFn: this.joinGroup
+              type: "group"
             }
           ]
         });
@@ -93,10 +98,12 @@ class Tundra extends React.Component {
 
   joinGroup() {
     console.log("requested")
+    this.toggle()
   }
 
   inviteHowl() {
     console.log("invited")
+    this.toggle()
   }
 
   componentDidMount() {
@@ -110,10 +117,10 @@ class Tundra extends React.Component {
       <Container>
         {/* Add conditional logic for rendering either this.state.data in ItemCards or the home page */}
         {this.props.currentDisplay === "home" ? (
-            <div>{/* Homepage */}</div>
+            <div className="tundra-home">{/* Homepage */}</div>
         ) : (
             this.state.data.map(card => {
-                return <ItemCard {...card} />
+                return <ItemCard key={card.id} {...card} />
         }))}
       </Container>
     );
