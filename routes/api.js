e0fc8c06f72edf1be9d9090eb18a5faafa7574a8
amp3193 <amp3193@yahoo.com>
2019-06-25 14:21:59 -0400

    post routes

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,26 @@
 
-const path = require("path")
+// const path = require("path")
 
-module.exports = function (app) {
+// module.exports = function (app) {
 
-    //POST route to create a new user
-    app.post("/api/user", function (req, res) {
-        console.log("adding this user:", req.body);
-        db.User.create(req.body)
-            .then(function (dbUser) {
-                res.json(dbUser);
-            });
-    });
+//     //POST route to create a new user
+//     app.post("/api/user", function (req, res) {
+//         console.log("adding this user:", req.body);
+//         db.User.create(req.body)
+//             .then(function (dbUser) {
+//                 res.json(dbUser);
+//             });
+//     });
 
-}
+// }
 
-app.get("/api/packs/:id", function (req, res) {;
-    db.Pack.findOne({
-        _id: req.params.id
-    })
-        .then(function (dbPack) {
-            res.json(dbPack);
-        });
-});
+// app.get("/api/packs/:id", function (req, res) {;
+//     db.Pack.findOne({
+//         _id: req.params.id
+//     })
+//         .then(function (dbPack) {
+//             res.json(dbPack);
+//         });
+// });
 
 
