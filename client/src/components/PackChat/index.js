7a0598c211ede3bc2c1cf3348adaad249927c977
Tyler Mathena <tgmathena@crimson.ua.edu>
2019-06-28 12:09:34 -0400

    chat bugfixes

--- client/src/components/PackChat/index.js
+++ client/src/components/PackChat/index.js
@@ -1,7 +1,13 @@
-import React from "react"
-import NewMessage from "../NewMessage"
-import { Container, ListGroup, FormInput, FormTextarea, Button } from "shards-react"
-import firebase from "firebase"
+import React from "react";
+import NewMessage from "../NewMessage";
+import {
+  Container,
+  ListGroup,
+  FormInput,
+  FormTextarea,
+  Button
+} from "shards-react";
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGsQy2KqnvRtOT7pZz8yIexudZyncUhX8",
@@ -16,21 +22,18 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const firebaseData = firebase.database();
 
-
 class PackChat extends React.Component {
-
   state = {
     messages: [],
     packRef: firebaseData.ref(this.props.packID),
     draft: ""
-  }
+  };
 
-  getMessages () {
+  getMessages() {
     //API call to (TODO) updated model to grab previous messages for pack with _id of _id
   }
 
   addMessage = () => {
-  
     /*
     var messageListRef = firebase.database().ref('message_list');
     var newMessageRef = messageListRef.push();
@@ -51,63 +54,81 @@ class PackChat extends React.Component {
       authorID: this.props.user._id,
       message: this.state.draft,
       time: Date.now()
-    })
-    const newMessages = this.state.messages
+    });
+    const newMessages = this.state.messages;
     //newMessages.push(/* message value */)
-    this.setState({
-      messages: newMessages
-    })
-  }
-
-  componentDidMount = () => {
-    
     this.state.packRef.on("value", data => {
-      const arrData = Object.values(data.val());
+      const arrData = Object.values(
+        data.val() || {
+          defaultMessage: {
+            author: "Wolf Pack Team",
+            authorID: 1,
+            message: "Be the first to chat in this pack!",
+            time: Date.now()
+          }
+        }
+      );
 
       this.setState({
         messages: arrData
-      })
-      console.log("State messages: ", this.state.messages)
+      });
+    });
+  };
 
+  componentDidMount = () => {
+    this.state.packRef.on("value", data => {
+      const arrData = Object.values(
+        data.val() || {
+          defaultMessage: {
+            author: "Wolf Pack Team",
+            authorID: 1,
+            message: "Be the first to chat in this pack!",
+            time: Date.now()
+          }
+        }
+      );
 
-    })
-
-  }
+      this.setState({
+        messages: arrData
+      });
+      console.log("State messages: ", this.state.messages);
+    });
+  };
 
   changeHandler = e => {
     this.setState({
       draft: e.target.value
-    })
-  }
+    });
+  };
 
-
-
-  render () {
-    return(
+  render() {
+    return (
       <Container>
         <ListGroup>
           {/* map this.state.messages to generate NewMessages */}
-          {this.state.messages.map( (message, i) => (
+          {this.state.messages.map((message, i) => (
             <NewMessage key={message.authorID + `${i}`} {...message} />
           ))}
-
         </ListGroup>
         <FormInput
-                id="draft"
-                name="draft"
-                placeholder="Chat"
-                type="text"
-                value={this.state.draft}
-                onChange={this.changeHandler}
-              />
-        <Button className="pill-button" theme="info" type="submit" onClick={this.addMessage} job="submit">
+          id="draft"
+          name="draft"
+          placeholder="Chat"
+          type="text"
+          value={this.state.draft}
+          onChange={this.changeHandler}
+        />
+        <Button
+          className="pill-button"
+          theme="info"
+          type="submit"
+          onClick={this.addMessage}
+          job="submit"
+        >
           Submit
         </Button>
       </Container>
-    )
+    );
   }
-
 }
-export default PackChat
-
-
+export default PackChat;
